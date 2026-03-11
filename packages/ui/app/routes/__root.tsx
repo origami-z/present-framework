ef9e686cfbfd8cb514a6efab812f469df18c4719
Zhihao Cui <5257855+origami-z@users.noreply.github.com>
2026-03-11 13:37:42 +0000

    Add SVG favicon for the Present UI app (#22)

--- packages/ui/app/routes/__root.tsx
+++ packages/ui/app/routes/__root.tsx
@@ -16,6 +16,11 @@ export const Route = createRootRoute({
     ],
     links: [
       {
+        rel: 'icon',
+        type: 'image/svg+xml',
+        href: '/favicon.svg',
+      },
+      {
         rel: 'stylesheet',
         href: 'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap',
       },
